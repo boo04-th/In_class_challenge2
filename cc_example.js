0b6976598acb83ab3818aed686a223904e1fbdfd
Hient <hient@usf.edu>
2025-02-11 16:07:42 -0500

    Task 1: Product Inventory Management

--- cc_example.js
+++ cc_example.js
@@ -1,1 +1,13 @@
 //Task 1
+let products = [
+    {name: "Laptop", price:1200, category:"Electronics"},
+    {name: "Phone", price:800, category:"Electronics"},
+    {name: "Desk Chair", price:150, category:"Furniture"},
+];
+
+// function getProductsByCategory (products_array,category_name );{
+//     return products_array.filter(product =>product.category === category_name);
+
+// }
+
+// console.log (getProductsByCategory(products, "Electronics"));
